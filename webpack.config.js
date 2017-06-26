aef024de1daedb96cc181bae4fd17313d5863c55
stiig <vasiliyrps2@gmail.com>
2017-06-26 11:25:37 +0500

    add rimraf and config for devServer

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    hot: true,
+    compress: true,
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin(),
+  ],
 };
