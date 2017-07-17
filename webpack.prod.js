38b874fdc5066d27bf1c527ead03608205ebc5ad
stiig <vasiliyrps2@gmail.com>
2017-07-17 18:39:38 +0500

    set NODE_ENV in production config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,2 +1,12 @@
 const Merge = require('webpack-merge');
 const CommonConfig = require('./webpack.common.js');
+
+module.exports = Merge(CommonConfig, {
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+  ]
+})
